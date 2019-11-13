2d5b0ae1987afc69c21da78e900568df5a1c8ccd
Kristian Windsor <kristianwindso@gmail.com>
2019-11-12 17:56:50 -0800

    added more sites and check for valid certificate

--- src/app.py
+++ src/app.py
@@ -3,10 +3,13 @@ import time
 import requests
 
 
-listOfWebsites = ['https://citethis.net/', 'https://12yearoldsimulator.com/']
+listOfWebsites = ['https://citethis.net/', 'https://12yearoldsimulator.com/', 'https://kristianwindsor.com/', 'https://passgen.site/', 'https://siliconvalleydrugs.com/']
 while True:
 	for site in listOfWebsites:
 		print(site)
-		r = requests.get(site)
-		print(r.status_code)
+		try:
+			r = requests.get(site)
+			print(r.status_code)
+		except:
+			print('expired cert')
 		time.sleep(5)
