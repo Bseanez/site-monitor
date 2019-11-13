cee62b86048025f94f460f8aa5e73ce183797e64
Brandon Seanez <brandonseanez@icloud.com>
2019-11-12 18:15:11 -0800

    created slack message def

--- src/app.py
+++ src/app.py
@@ -3,6 +3,18 @@ import time
 import requests
 
 
+#send slack message 
+def slackmessage():
+	print("hi")
+	#slack_token = os.environ["SLACK_API_TOKEN"]
+				#client = slack.WebClient(token=slack_token)
+
+				#client.chat_postMessage(
+				 # channel="site",
+				 # text="404 error"
+				#)
+
+
 listOfWebsites = ['https://citethis.net/', 'https://12yearoldsimulator.com/', 'https://kristianwindsor.com/', 'https://passgen.site/', 'https://siliconvalleydrugs.com/']
 while True:
 	for site in listOfWebsites:
@@ -10,6 +22,9 @@ while True:
 		try:
 			r = requests.get(site)
 			print(r.status_code)
+			if r.status_code != 200:
+				slackmessage()
 		except:
 			print('expired cert')
+			slackmessage()
 		time.sleep(5)
