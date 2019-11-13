e491828de1d952637583f40f1a387d174a746d03
Brandon Seanez <brandonseanez@icloud.com>
2019-11-12 17:46:00 -0800

    finished app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python 
 import time
+import requests
 
+
+listOfWebsites = ['https://citethis.net/', 'https://12yearoldsimulator.com/']
 while True:
-    time.sleep(1)
+	for site in listOfWebsites:
+		print(site)
+		r = requests.get(site)
+		print(r.status_code)
+		time.sleep(5)
