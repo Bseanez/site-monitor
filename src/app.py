32f4e2d42af4d14ed9b388a4cb278466d4c59c15
Brandon Seanez <brandonseanez@icloud.com>
2019-11-13 08:53:28 -0800

    updated app.py

--- src/app.py
+++ src/app.py
@@ -6,17 +6,18 @@ import os
 
 
 #send slack message 
-def slackmessage():
+def slackmessage(message):
 	data = {
 		'token': os.environ['SLACK_TOKEN'],
 		'channel': 'uptimebot',
-		'text': 'hello'
+		'text': message
 	}
 	headers = {
 		'Content-type': 'application/json'
 	}
 	r = requests.post(os.environ['SLACK_URL'], data=json.dumps(data), headers=headers)
 	print(r)
+
 
 
 listOfWebsites = ['https://citethis.net/', 'https://12yearoldsimulator.com/', 'https://kristianwindsor.com/', 'https://passgen.site/', 'https://siliconvalleydrugs.com/']
@@ -27,8 +28,8 @@ while True:
 			r = requests.get(site)
 			print(r.status_code)
 			if r.status_code != 200:
-				slackmessage()
+				slackmessage(site)
 		except:
 			print('expired cert')
-			slackmessage()
-		time.sleep(20)
+			slackmessage(site)
+			
